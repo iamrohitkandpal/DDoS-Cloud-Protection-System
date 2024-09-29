e934f10d143015826706c1380bc934ffab8d1a8e
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-09-30 00:51:45 +0530

    MongoDB Connection Successfull

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,27 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDb from './utils/database.js';
+import bodyParser from "body-parser";
+
 dotenv.config({});
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended:true}));
+const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        message: "Backend Relation"
+    })
+})
 
 const PORT = 8000;
 app.listen(PORT, ()=>{
