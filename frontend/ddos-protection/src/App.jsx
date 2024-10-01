7a549ae85617f3fff87e92743956b113d68732ed
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-10-01 23:07:22 +0530

    Initialized the Frontend of the Project

---
+++ frontend/ddos-protection/src/App.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <header className='App-header'>
+          <div className='font-bold text-6xl'>Hello World</div>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
