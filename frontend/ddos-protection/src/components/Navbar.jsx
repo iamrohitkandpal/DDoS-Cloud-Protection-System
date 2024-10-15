3eb30d2ff5c63fec62a1fc856cf45a318b46fc7d
Rohit Kandpal <iamrohitkandpal@gmail.com>
2024-10-16 00:01:07 +0530

    Frontend Navbar

--- frontend/ddos-protection/src/components/Navbar.jsx
+++ frontend/ddos-protection/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+import logo from '../assets'
+
 const Navbar = () => {
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
             <div className="flex justify-center items-center">
-                
+                <div className="flex items-center flex-shrink-0">
+                  <img src={logo} alt="" />
+                </div>
             </div>
         </div>
     </nav>
